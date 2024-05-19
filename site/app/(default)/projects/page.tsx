b37d39f1c54243c92ccc338077489a400ffcb402
Steven Hilton <mshiltonj@gmail.com>
2024-05-19 19:54:29 -0400

    resource routing update

--- site/app/(default)/projects/page.tsx
+++ site/app/(default)/projects/page.tsx
@@ -4,13 +4,25 @@ export default function Page(){
   <h1>Projects</h1>
 
   <div>
+    <h2>Resource Routing</h2>
+    <div>
+      A npm  package provides a way to a build simple, easy restful route suite for nodejs & expressjs, with a viewable routing table.
+      Github: <a href="https://github.com/mshiltonj/resource-routing">https://github.com/mshiltonj/resource-routing</a>
+      NPM: <a href="https://www.npmjs.com/package/resource-routing">https://www.npmjs.com/package/resource-routing</a>
+    </div>
+
+
     <h2>Zombie Infestation Simulator</h2>
-    A small project made using Godot, where you watch the spread of a zombie infestation.
-    <a href="https://pengurrito.itch.io/zombie-infestation-simulator">on itch.io</a>
+    <div>
+      A small project made using Godot, where you watch the spread of a zombie infestation.
+      <a href="https://pengurrito.itch.io/zombie-infestation-simulator">on itch.io</a>      
+    </div>
 
     <h2>Software Wars</h2>
-    A very, very old idea of creating a map of the waring factions of the software world. I did not create the original maps, but I did create some of the later ones. This is more more or less an archive.
-    <a href="/software_wars">Software Wars</a>
+    <div>
+      A very, very old idea of creating a map of the waring factions of the software world. I did not create the original maps, but I did create some of the later ones. This is more more or less an archive.
+      <a href="/software_wars">Software Wars</a>
+    </div>
   </div>
 
   </div>
